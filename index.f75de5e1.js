86aed9b0a43dc7298e6e7d4feec2f6369b8aeb06
Artem-Brui <artembryj@gmail.com>
2024-09-06 19:15:31 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+// write your code here
+const elements = {
+    countriesList: [
+        ...document.querySelectorAll(".population")
+    ],
+    totalPopulation: document.querySelector(".total-population"),
+    averagePopulation: document.querySelector(".average-population")
+};
+const totalPopulation = elements.countriesList.reduce((total, country)=>{
+    const population = country.textContent;
+    return total + parseInt(population.replace(/,/g, ""), 10);
+}, 0);
+const averagePopulation = totalPopulation / elements.countriesList.length;
+elements.totalPopulation.textContent = totalPopulation.toLocaleString();
+elements.averagePopulation.textContent = averagePopulation.toLocaleString();
+
+//# sourceMappingURL=index.f75de5e1.js.map
